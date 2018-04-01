035fb845d6e88885707cfba3a4a6b151c83b15ab
phamquanghuy97 <36045157+phamquanghuy97@users.noreply.github.com>
2018-04-02 00:47:42 +0700

    update

---
+++ firtweb/home/conect_db.php
@@ -0,0 +1,13 @@
+<?php
+	$servername = 'localhost';
+	$username = 'root';
+	$password = '12031997';
+	$db = 'bltweb';
+	$conn = mysqli_connect($servername, $username, $password, $db);
+
+	// Check connection
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
+		echo "Connected successfully";
+?>
